d30cdbfd1f68c97aa15042f0f19a6a9642d707a4
dannypas00 <mods@dannypas.nl>
2024-05-21 21:20:59 +0200

    Gold from diamond recipe set up, but consumes stone

--- src/main/java/com/dannypas00/simplealchemy/registerables/items/PhilosophersStoneItem.java
+++ src/main/java/com/dannypas00/simplealchemy/registerables/items/PhilosophersStoneItem.java
@@ -4,16 +4,19 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 
 public class PhilosophersStoneItem extends AbstractItem {
+    private static final int maxDurability = 128;
+
     public PhilosophersStoneItem() {
         super(PhilosophersStoneItem.getProperties());
     }
 
     public static Properties getProperties() {
-        return new Item.Properties()
+        return new Properties()
                 .stacksTo(1)
                 .fireResistant()
                 .setNoRepair()
-                .rarity(Rarity.RARE);
+                .rarity(Rarity.RARE)
+                .durability(maxDurability);
     }
 
     public static PhilosophersStoneItem register() {
